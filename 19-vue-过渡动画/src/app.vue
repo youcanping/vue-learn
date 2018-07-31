d8af935969c7b065062ba6d97a6b276bedfb6f7a
youcanping <youcanping2008@163.com>
2018-07-31 18:00:04 +0800

    commit

---
+++ 19-vue-过渡动画/src/app.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<button v-on:click="isShow = !isShow">按钮</button>
+		<transition name="fade">
+			<p v-if="isShow"></p>
+		</transition>
+	</div>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				isShow: true
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	p{
+		width: 100px;
+		height: 100px;
+		background: blue;
+	}
+	.fade-enter-active, .fade-leave-active{
+		transition: opacity 0.5s;
+	}
+	.fade-enter, .fade-leave-to{
+		opacity: 0;
+	}
+</style>
