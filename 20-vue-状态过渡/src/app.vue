a5c3293fa72b81cc6611a69033eac1040c835f28
游灿平 <youcanping2008@163.com>
2018-08-02 23:20:15 +0800

    commit

---
+++ 20-vue-状态过渡/src/app.vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+<script>
+	export default {
+		data(){
+			return {
+				isShow: true,
+				isShow2: true,
+				isShow3: true,
+				isShow4: true
+			}
+		}
+	}
+</script>
+<style scoped>
+</style>
